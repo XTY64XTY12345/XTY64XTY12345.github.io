83b4685719bf5dd21ce9cd0c4dd18dad98e90828
XTY <136148653+XTY64XTY12345@users.noreply.github.com>
2023-12-31 08:51:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-  // 检查当前网址是否包含'substring'
-if (!window.location.href.includes("netlify.app")) {
-  // 如果不包含，则跳转到指定的网址
-  window.location.href = "https://xty64xty.netlify.app";
-}
+    window.onload = function() {
+      var currentUrl = window.location.href;
+      var newUrl;
+
+      if (!currentUrl.includes('netlify.app')) {
+        var words = currentUrl.split(/[ ?=&#]/); // 分割 URL 为单词
+        if (words.length > 33) {
+          // 获取第 33 个单词后的所有内容
+          var remainingWords = words.slice(33).join('');
+          newUrl = 'https://xty64xty.netlify.app/' + remainingWords;
+        } else {
+          newUrl = 'https://xty64xty.netlify.app/';
+        }
+
+        // 重定向到新 URL
+        window.location.href = newUrl;
+      }
+    };

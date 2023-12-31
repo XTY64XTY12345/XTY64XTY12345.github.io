331d01b86f7c31dc8a2135b0d33eebd9f50df102
XTY <136148653+XTY64XTY12345@users.noreply.github.com>
2023-12-31 08:57:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
     window.onload = function() {
       var currentUrl = window.location.href;
-      var newUrl;
+      var baseUrl = 'https://xty64xty.netlify.app';
 
       if (!currentUrl.includes('netlify.app')) {
-        var words = currentUrl.split(/[ ?=&#]/); // 分割 URL 为单词
-        if (words.length > 33) {
-          // 获取第 33 个单词后的所有内容
-          var remainingWords = words.slice(33).join('');
-          newUrl = 'https://xty64xty.netlify.app/' + remainingWords;
-        } else {
-          newUrl = 'https://xty64xty.netlify.app/';
-        }
+        var urlParts = new URL(currentUrl);
+        var newPath = urlParts.pathname + urlParts.search + urlParts.hash;
+        var newUrl = baseUrl + newPath;
 
         // 重定向到新 URL
         window.location.href = newUrl;

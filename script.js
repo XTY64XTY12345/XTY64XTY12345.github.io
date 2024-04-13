77357943bc6c890738eac50ccda75787095d2cb5
XTY <136148653+XTY64XTY12345@users.noreply.github.com>
2024-04-13 13:01:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-    window.onload = function() {
-      var currentUrl = window.location.href;
-      var baseUrl = 'https://xty64xty.netlify.app';
+//    window.onload = function() {
+//      var currentUrl = window.location.href;
+//      var baseUrl = 'https://xty64xty.netlify.app';
 
-      if (!currentUrl.includes('netlify.app')) {
-        var urlParts = new URL(currentUrl);
-        var newUrl = baseUrl + location.pathname;
+//      if (!currentUrl.includes('netlify.app')) {
+//        var urlParts = new URL(currentUrl);
+//        var newUrl = baseUrl + location.pathname;
 
-        // 重定向到新 URL
-        window.location.href = newUrl;
-      }
-    };
+//        // 重定向到新 URL
+//        window.location.href = newUrl;
+//      }
+//    };
